d1365e937b0a65972bf16bb64cd0a730b591221e
PriyalWa <PriyalWa@PAL-ICT-PRIYAL>
2018-04-06 03:49:15 +0530

    Initial Commit

---
+++ StudentSample/Migrations/20180405213345_InitStudent.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace StudentSample.Migrations
+{
+    public partial class InitStudent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    StudentId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Address = table.Column<string>(maxLength: 100, nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Name = table.Column<string>(maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.StudentId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
---
+++ StudentSample/Models/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentSample.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Display(Name = "Student Name")]
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [Display(Name = "Address")]
+        [StringLength(100)]
+        public string Address { get; set; }
+
+        [Display(Name = "Age")]
+        [Range(5,18)]
+        public int Age { get; set; }
+
+    }
+}
